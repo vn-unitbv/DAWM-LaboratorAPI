2a365dc2d86f851d6e455da7338f268bbed8f8fe
Teodor Ghinea <ionutteo972@gmail.com>
2023-03-02 10:57:40 +0200

    Initial Commit

---
+++ Project/Settings/Dependencies.cs
@@ -0,0 +1,20 @@
+﻿namespace Project.Settings
+{
+    public static class Dependencies
+    {
+
+        public static void Inject(WebApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.Services.AddControllers();
+            applicationBuilder.Services.AddSwaggerGen();
+
+            AddServices(applicationBuilder.Services);
+        }
+
+        private static void AddServices(IServiceCollection services)
+        {
+
+        }
+
+    }
+}
---
+++ TestDrive/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+using static TestDrive.DelegateExamples;
+
+namespace TestDrive;
+
+public class Program
+{
+    public const int number = 10;
+    /*
+     * string result;
+
+        var delegateExamples = new DelegateExamples();
+        var myDelegate = new MyMethodDelegate(delegateExamples.MyMethod);
+
+        result = myDelegate(number);
+        Console.WriteLine(result);
+
+        myDelegate = delegateExamples.MySecondMethod;
+
+        result = myDelegate(number);
+        Console.WriteLine(result);
+     */
+    public static void Main(string[] args)
+    {
+        // IEnumerable
+
+        List<string> strings = new List<string>() { "a", "b", "c", "d", "e"};
+
+        var result = strings.ParseList();
+
+    }
+}
+
+public static class ExtensionMethods
+{
+    public static IEnumerable<string> ParseList(this List<string> strings)
+    {
+        return strings
+            .Where(x => x != "a")
+            .Where(x => x != "b")
+            .Where(x => x != "c")
+            .Select(x => x + "_")
+            .Select(x => x.ToUpper())
+            .OrderByDescending(x => x);
+    }
+}
