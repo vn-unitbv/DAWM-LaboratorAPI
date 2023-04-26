3cb116400f14ce41193636ea81bb6dcadb9c9b6a
Vitályos Norbert <vitalyosn@gmail.com>
2023-04-26 21:31:10 +0300

    added User entity

---
+++ DataLayer/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace DataLayer.Entities;
+
+public class User : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
