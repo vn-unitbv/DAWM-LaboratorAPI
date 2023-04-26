290b9c0c8b89fdefd8dd61faee958e416696f7f1
Vitályos Norbert <vitalyosn@gmail.com>
2023-04-26 22:19:54 +0300

    made Student entity inherit from User entity

--- DataLayer/Entities/Student.cs
+++ DataLayer/Entities/Student.cs
@@ -1,20 +1,9 @@
 ﻿namespace DataLayer.Entities;
 
-public class Student : BaseEntity
+public class Student : User
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public DateTime DateOfBirth { get; set; }
-
     public int ClassId { get; set; }
     public Class Class { get; set; }
     public List<Grade> Grades { get; set; }
-
-    public string Address { get; set; }
-
-
-    public string Email { get; set; }
-
-    public string PasswordHash { get; set; }
 }
 
