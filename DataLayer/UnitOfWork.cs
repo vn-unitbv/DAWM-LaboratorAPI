03ac5f9406f78d58aa51f7e627177fa2cbb866da
Vitályos Norbert <vitalyosn@gmail.com>
2023-04-26 22:46:24 +0300

    added GradesRepository in UnitOfWork

--- DataLayer/UnitOfWork.cs
+++ DataLayer/UnitOfWork.cs
@@ -7,6 +7,7 @@ namespace DataLayer
         public StudentsRepository Students { get; }
         public ClassRepository Classes { get; }
         public ProfessorsRepository Professors { get; }
+        public GradesRepository Grades { get; }
 
         private readonly AppDbContext _dbContext;
 
@@ -15,13 +16,15 @@ namespace DataLayer
             AppDbContext dbContext,
             StudentsRepository studentsRepository,
             ClassRepository classes,
-            ProfessorsRepository professorsRepository
+            ProfessorsRepository professorsRepository,
+            GradesRepository gradesRepository
         )
         {
             _dbContext = dbContext;
             Students = studentsRepository;
             Classes = classes;
             Professors = professorsRepository;
+            Grades = gradesRepository;
         }
 
         public void SaveChanges()
