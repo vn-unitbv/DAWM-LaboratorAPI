277498bd79dc7007eb21d53f95548a0b8eb2eb33
Vitályos Norbert <vitalyosn@gmail.com>
2023-04-26 21:24:51 +0300

    added GradesController

---
+++ Project/Controllers/GradesController.cs
@@ -0,0 +1,20 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    [ApiController]
+    [Route("api/grades")]
+    [Authorize]
+    public class GradesController : ControllerBase
+    {
+        private GradeService gradeService { get; set; }
+
+
+        public GradesController(GradeService gradeService)
+        {
+            this.gradeService = gradeService;
+        }
+    }
+}
