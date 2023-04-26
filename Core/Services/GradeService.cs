67ba99d9dda973d9cdd38b49b645323e29fbbb56
Vitályos Norbert <vitalyosn@gmail.com>
2023-04-26 21:21:21 +0300

    added GradeService

---
+++ Core/Services/GradeService.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class GradeService
+    {
+        private readonly UnitOfWork unitOfWork;
+
+        public GradeService(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
